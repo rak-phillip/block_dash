6c91f97b1ce9c3bd094ca1ffa57ef4583c6f8f15
Phillip Rak <rak.phillip@gmail.com>
2015-03-21 15:19:21 -0700

    --Added actions to the enemy object
    Added mover rules for the enemy
    Added colision and destruction rules for the enemy object

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D movingObject;
+
+	// Use this for initialization
+	void Start () {
+	
+		movingObject = GetComponent<Rigidbody2D> ();
+
+		movingObject.velocity = new Vector2(-10f, 0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
