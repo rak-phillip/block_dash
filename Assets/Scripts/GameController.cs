2d71c12da08514f751f7521d0bf83dd42477feaf
Phillip Rak <rak.phillip@gmail.com>
2015-03-21 17:03:17 -0700

    --Set the enemies to spawn in waves

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour {
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnTimer;
+	public float waveTimer;
 
 	// Use this for initialization
 	void Start () {
@@ -20,14 +21,17 @@ public class GameController : MonoBehaviour {
 
 	IEnumerator spawnWaves () {
 
-		for (int x = 0; x < hazardCount; x++) {
-			Vector3 spawnPosition = new Vector3 (spawnValues.x, Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
-			//Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-			Quaternion spawnRotation = Quaternion.identity;
+		while (true) {
+			for (int x = 0; x < hazardCount; x++) {
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
+				//Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
 			
-			Instantiate (enemy, spawnPosition, spawnRotation);
+				Instantiate (enemy, spawnPosition, spawnRotation);
 
-			yield return new WaitForSeconds(spawnTimer);
+				yield return new WaitForSeconds (spawnTimer);
+			}
+			yield return new WaitForSeconds (waveTimer);
 		}
 	}
 
