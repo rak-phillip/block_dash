e12c87d77eb4cf4e86c1743198a3a2c1aa3057c6
Phillip Rak <rak.phillip@gmail.com>
2015-03-22 23:18:12 -0700

    --Added audio to the project

---
+++ Assets/Scripts/MusicHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicHandler : MonoBehaviour {
+
+	private static MusicHandler instance = null;
+
+	public static MusicHandler Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PowerUpController : MonoBehaviour {
 
 	private GameController gameController;
+	public AudioSource pickupAudio;
 
 	// Use this for initialization
 	void Start () {
@@ -25,6 +26,7 @@ public class PowerUpController : MonoBehaviour {
 		if (other.tag.Equals("Player")) {
 			Destroy(gameObject);
 			gameController.AddScore(300);
+			this.pickupAudio.Play();
 			return;
 		}
 	}
